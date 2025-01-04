8909bfa0af42d670681d1967df421510db320d28
cllown <artoorclownxd@gmail.com>
2025-01-04 20:09:18 +0200

    fix

--- vite-tonconnect/src/App.tsx
+++ vite-tonconnect/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+/* import { useState } from 'react' */
 import './App.css'
 import { TonConnectButton } from '@tonconnect/ui-react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+/*   const [count, setCount] = useState(0) */
 
   return (
 	<div className='header'>
